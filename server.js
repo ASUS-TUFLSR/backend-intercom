3a8f3a1682b18f2819feb5ebad017030c15a1184
Manmit Pal <manmitpal348@gmail.com>
2025-05-24 21:51:47 +0530

    forgot to remove import statement

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const geminiRouter = require('./routes/gemini');
-import key from "./config/intercom-admin-panel-5d91cfc0aa57.json"
 
 
 
